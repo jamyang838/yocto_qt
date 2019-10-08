d90a4a48d09134d08fe4b900a06b88c2822ab66d
jamyang838 <jamyang838@icloud.com>
2019-10-08 01:33:19 -0700

    ~

--- qt_project/sample3/main.cpp
+++ qt_project/sample3/main.cpp
@@ -2,23 +2,29 @@
 #include <QQmlApplicationEngine>
 #include <wiringPi.h>
 #include <wiringSerial.h>
+#include <wiringPiSPI.h>
+
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 
     QGuiApplication app(argc, argv);
 /*********************************************************/
+    int chan=1;
+    int speed=1000000;
+     wiringPiSPISetup (chan, speed);
+
    //**************************
     int handle = serialOpen ("/dev/ttyS0", 115200) ;
-
     //***************************
     wiringPiSetup() ;
     pinMode (0, OUTPUT) ;
+
     for (;;)
     {
         serialPuts (handle, "asd") ;
-        digitalWrite (0, HIGH) ; delay (500) ;
-        digitalWrite (0,  LOW) ; delay (500) ;
+        digitalWrite (0, HIGH) ; delay (20) ;
+        digitalWrite (0,  LOW) ; delay (10) ;
     }
  /* ******************************************************/
     QQmlApplicationEngine engine;
